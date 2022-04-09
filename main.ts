f191edc2d6961bba1dbd8af0cca2ec6c82249752
David Sherret <dsherret@gmail.com>
2022-04-09 10:25:07 -0400

    Update to latest version.

--- main.ts
+++ main.ts
@@ -2,4 +2,4 @@ import { Project } from "ts-morph";
 
 const project = new Project();
 const sourceFile = project.createSourceFile("test.ts", "class MyClass {}")
-console.log(sourceFile.getClassOrThrow("MyClass").getName());
+console.log(sourceFile.getClasses()[0].getName());
